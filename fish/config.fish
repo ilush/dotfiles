da71b56fae0d542735747df5b6ecc885f8be263d
Illia Polliul <ilush8@gmail.com>
2024-05-21 15:22:03 +0200

    Fix brew imports

--- fish/config.fish
+++ fish/config.fish
@@ -19,6 +19,18 @@ set -x LC_ALL en_US.UTF-8
 set -x VIRTUAL_ENV_DISABLE_PROMPT off
 set PATH ~/bin /usr/local/bin/ $PATH
 
+if test -d /home/linuxbrew/.linuxbrew # Linux
+	set -gx HOMEBREW_PREFIX "/home/linuxbrew/.linuxbrew"
+	set -gx HOMEBREW_CELLAR "$HOMEBREW_PREFIX/Cellar"
+	set -gx HOMEBREW_REPOSITORY "$HOMEBREW_PREFIX/Homebrew"
+else if test -d /opt/homebrew # MacOS
+	set -gx HOMEBREW_PREFIX "/opt/homebrew"
+	set -gx HOMEBREW_CELLAR "$HOMEBREW_PREFIX/Cellar"
+	set -gx HOMEBREW_REPOSITORY "$HOMEBREW_PREFIX/homebrew"
+end
+fish_add_path -gP "$HOMEBREW_PREFIX/bin" "$HOMEBREW_PREFIX/sbin";
+! set -q MANPATH; and set MANPATH ''; set -gx MANPATH "$HOMEBREW_PREFIX/share/man" $MANPATH;
+! set -q INFOPATH; and set INFOPATH ''; set -gx INFOPATH "$HOMEBREW_PREFIX/share/info" $INFOPATH;
 
 # set private environment variables stored outsited source control
 test -r ~/.fish.env; and export (cat ~/.fish.env|xargs -L 1)
